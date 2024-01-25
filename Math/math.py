922f6c0819d432dc3e636dddb2f4d11623028acd
Sydney Hribar <hribar.sydney@gmail.com>
2024-01-25 09:03:54 -0500

    Math

---
+++ Math/math.py
@@ -0,0 +1,12 @@
+#Programmer: Sydney Hribar
+#Program: Math
+#Date: 1.10.2024
+
+print(9 * 9) #Multiply
+print(9 + 9) #Addition
+print(9 - 6) #Subtract
+print(9 / 3) #Divide
+print(50 ** 3) #Exponent - the ** makes the next number an exponent 50^3
+print(50 % 6) #modulas it will print 2 because its the remainder 50/6 is 8 because 6 goes into 50 8 times and 6*8 is 48 and there is 2 left over
+print(50 / 6) #Division with remainder - float
+print(50 // 6) #The // will give a whole number without a remainder/ a decimal 
