c32e13638fcf03144b3dc3ac582a4d9156ded90b
Sydney Hribar <hribar.sydney@gmail.com>
2024-01-25 09:01:37 -0500

    Strings

---
+++ Strings/first.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+#Programmer: Sydney Hribar
+#Program: Strings
+
+print("Hello World\n") #\n skips a line so its neater and seperated
+
+print("7 + 8 =", 7+8) #if you want to seperate these two lines put an empty print line function: print("")
+print("9 + 3 = "+ str(9+3), "\n") # lor you can put \n in quotes after
+
+print("""Happy New Year!
+Have a great year everyone!""") #"""/ triple quotes allows you to print on multiple lines
+
